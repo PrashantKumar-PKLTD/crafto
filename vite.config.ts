bc4c83e675bb01d236cd037e2d15718d2d3eadaa
PrashantKumar-PKLTD <connect@prashantkumar.co.uk>
2025-08-30 21:31:31 +0530

    Configure Vite to use HTTP

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: true,
+    https: false,
     host: 'localhost',
     port: 5173
   },
